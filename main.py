4e6298ffc31a93283d519ccc06cb0151e4ebb619
Woodongsik <woodongsik@gmail.com>
2021-05-21 13:39:02 +0800

    # I added some comments to explain about this class

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from calculator import CalculatorWindow
 
+# This is where the calculator begins
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     calculator = CalculatorWindow()
